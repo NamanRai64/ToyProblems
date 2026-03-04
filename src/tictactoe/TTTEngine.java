f1781548c81277d60bbedb45c85eb53b3d1776ef
SidHarshal <sidhanaharshal@gmail.com>
2026-03-05 03:56:42 +0530

    Add TTTEngine Draw Feature

--- src/tictactoe/TTTEngine.java
+++ src/tictactoe/TTTEngine.java
@@ -9,65 +9,80 @@ class TTTEngine {
         int n = arr.length;
         boolean move = false;
         while (!TTTValidator.validate(arr) && i < n * n) {
-            //Player 1'X'
+            // Player 1'X'
             System.out.println();
             System.out.println();
             System.out.println("Player 1(X):");
             move = false;
             while (!move) {
                 int choice = sc.nextInt();
-                int row = (choice-1)/n;
-                int col = (choice-1)%n;
-                if (arr[row][col] == '_') {
-                    arr[row][col] = 'X';
-                    move = true;
+                int row = (choice - 1) / n;
+                int col = (choice - 1) % n;
+                if (choice > 0 && choice <= n * n) {
 
-                } else {
-                    System.out.println("Invalid Move");
-                    move = false;
+                    if (arr[row][col] == '_') {
+                        arr[row][col] = 'X';
+                        move = true;
+
+                    } else {
+                        System.out.println("Invalid Move");
+                        move = false;
+                    }
                 }
             }
 
             print(arr);
-            if(TTTValidator.validate(arr)){
+            if (TTTValidator.validate(arr)) {
                 System.out.println("Player 1 Wins!!!");
                 break;
+            }
+            if (TTTValidator.draw(arr)) {
+                System.out.println("Draw");
+                return;
             }
             System.out.println();
             System.out.println();
             i++;
 
-            //Player 2'O'
+            // Player 2'O'
             System.out.println("Player 2(O):");
             move = false;
             while (!move) {
 
                 int choice = sc.nextInt();
-                int row = (choice-1)/n;
-                int col = (choice-1)%n;
-                if (arr[row][col] == '_') {
-                    arr[row][col] = 'O';
-                    move = true;
-                } else {
-                    System.out.println("Invalid Move");
-                    move = false;
+                int row = (choice - 1) / n;
+                int col = (choice - 1) % n;
+                if (choice > 0 && choice <= n * n) {
+
+                    if (arr[row][col] == '_') {
+                        arr[row][col] = 'O';
+                        move = true;
+                    } else {
+                        System.out.println("Invalid Move");
+                        move = false;
+                    }
                 }
             }
             print(arr);
-            if(TTTValidator.validate(arr)){
+            if (TTTValidator.validate(arr)) {
                 System.out.println("Player 2 Wins!!!");
                 break;
             }
+            if (TTTValidator.draw(arr)) {
+                System.out.println("Draw");
+                return;
+            }
             i++;
         }
-        if(!TTTValidator.validate(arr)){
-            System.out.println("Draw");
-        }
+        // if (!TTTValidator.validate(arr)) {
+        // System.out.println("Draw");
+        // }
     }
+
     public static void print(char[][] arr) {
         int n = arr.length;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
@@ -75,11 +90,10 @@ class TTTEngine {
 
     }
 
-    public static void intitTTT(String [] args, int n){
-        //Initialize
-        char[][] arr = new char[n][n];
-        for(int i = 0; i < n; i++){
-            for( int j = 0; j < n; j++){
+    public static void initTTT(char[][] arr, int n) {
+        // Initialize
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 arr[i][j] = '_';
             }
         }
@@ -87,4 +101,6 @@ class TTTEngine {
         runTwoPlayer(arr);
 
     }
+
+    
 }
--- src/tictactoe/TTTValidator.java
+++ src/tictactoe/TTTValidator.java
@@ -59,4 +59,23 @@ public class TTTValidator {
 
         return false;
     }
+    
+   public static boolean draw(char[][] arr){
+
+    int n = arr.length;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(arr[i][j] == '_'){
+                return false;   
+            }
+        }
+    }
+
+    if(!validate(arr)){
+        return true;  
+    }
+
+    return false;
+}
 }
