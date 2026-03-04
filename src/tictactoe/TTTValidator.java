e532e3119dc2b7b15c0de1bc705142a7878dcf07
SidHarshal <sidhanaharshal@gmail.com>
2026-03-05 01:44:40 +0530

    Add Engine For TTT

--- src/tictactoe/TTTValidator.java
+++ src/tictactoe/TTTValidator.java
@@ -1,65 +1,62 @@
 package tictactoe;
 
 public class TTTValidator {
-    public static boolean validate(String[][] arr){
-        
-            int n = arr.length;
-            //Row
-            for(int i = 0; i < n; i++){
-                boolean v = true;
-                for(int j = 0; j < n; j++){
-                     if(!(arr[i][j].equals(arr[i][0]))){
-                        v = false;
-                         break;
-                     }
-                    
-                }
-                if(v){
-                    return true;
-                }
-            }
+    public static boolean validate(char[][] arr){
 
-            //Column
-            for(int i = 0; i < n; i++){
-                boolean v = true;
-                for(int j = 0; j < n; j++){
-                     if(!arr[j][i].equals(arr[0][i])){
-                     v = false;
-                          break;
-                     }
-                    
-                }
-                if(v){
-                    return true;
-                }
-            }
-            
-            //Main Diagonal
+        int n = arr.length;
+
+        // Row
+        for(int i = 0; i < n; i++){
             boolean v = true;
-            for(int i = 0; i < n; i++){
-                if(!(arr[i][i].equals(arr[0][0]))){
+            for(int j = 0; j < n; j++){
+                if(arr[i][j] != arr[i][0]){
                     v = false;
                     break;
                 }
             }
-            if(v){
+            if(v && arr[i][0] != '_'){
                 return true;
             }
+        }
 
-            //Anti Diagonal
-            v = true;              
-            for(int i = 0; i < n; i++){
-                if(!(arr[i][n-1-i].equals(arr[0][n-1]))){
+        // Column
+        for(int i = 0; i < n; i++){
+            boolean v = true;
+            for(int j = 0; j < n; j++){
+                if(arr[j][i] != arr[0][i]){
                     v = false;
                     break;
                 }
-            
             }
-            if(v){
+            if(v && arr[0][i] != '_'){
                 return true;
             }
-            else
-                return false;
+        }
+
+        // Main Diagonal
+        boolean v = true;
+        for(int i = 0; i < n; i++){
+            if(arr[i][i] != arr[0][0]){
+                v = false;
+                break;
+            }
+        }
+        if(v && arr[0][0] != '_'){
+            return true;
+        }
+
+        // Anti Diagonal
+        v = true;
+        for(int i = 0; i < n; i++){
+            if(arr[i][n-1-i] != arr[0][n-1]){
+                v = false;
+                break;
+            }
+        }
+        if(v && arr[0][n-1] != '_'){
+            return true;
+        }
+
+        return false;
     }
-    
 }
